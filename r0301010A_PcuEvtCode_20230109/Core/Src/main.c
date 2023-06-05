b00630adc987821177e8f6a79b96a6d636e4a23b
JeffChiaNingChang <chia-ning.chang@brp.com>
2023-06-05 14:36:04 +0800

    Disable PWM break at main initital

--- r0301010A_PcuEvtCode_20230109/Core/Src/main.c
+++ r0301010A_PcuEvtCode_20230109/Core/Src/main.c
@@ -1070,6 +1070,7 @@ static void MX_TIM20_Init(void)
   sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
   sBreakDeadTimeConfig.DeadTime = 128;
 //  sBreakDeadTimeConfig.BreakState = TIM_BREAK_ENABLE;
+  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
   sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_LOW;
   sBreakDeadTimeConfig.BreakFilter = 0;
   sBreakDeadTimeConfig.BreakAFMode = TIM_BREAK_AFMODE_INPUT;
