84d862fb156e07150c72ab9469e40a831b49720d
yangwi1987 <will.yang@brp.com>
2024-02-05 15:53:42 +0800

    correct as comment: 1. replace equal with greater/less operator when dealing with float 2. modify the code in Drive_vehicleStateMachine() at case "drive" and "warning" to fit doc

--- r04010101_ESCAPP_20231004/Core/Src/Drive.c
+++ r04010101_ESCAPP_20231004/Core/Src/Drive.c
@@ -1811,7 +1811,7 @@ void Drive_VehicleStateMachine( void )
       {
         if ((Btn_StateRead(BTN_IDX_BST_BTN)== BTN_STATE_HIGH) &&
             (Btn_StateRead(BTN_IDX_REV_BTN)== BTN_STATE_HIGH) &&
-            (Axis[0].ThrotMapping.PercentageTarget == 0 ))
+            (Axis[0].ThrotMapping.PercentageTarget < 0.01 ))
         {
           DualBtnTimeCnt ++;
 
@@ -1833,9 +1833,12 @@ void Drive_VehicleStateMachine( void )
 
       // error situation
       if( ESCMainState == ESC_OP_ALARM || Bat_MainSMGet() == BAT_MAIN_ALARM )
-      //if( Bat_MainSMGet() == BAT_MAIN_ALARM)
       {
         EnterVehicleAlarmState();
+      }
+	  else if( Btn_StateRead(BTN_IDX_KILL_SW) == BTN_STATE_HIGH)
+      {
+        EnterVehicleStandbyState();
       }
       else if( ESCMainState == ESC_OP_LIMPHOME)
       {
@@ -1844,10 +1847,6 @@ void Drive_VehicleStateMachine( void )
       else if( ESCMainState == ESC_OP_WARNING)
       {
         EnterVehicleWarningState();
-      }
-      else if( Btn_StateRead(BTN_IDX_KILL_SW) == BTN_STATE_HIGH)
-      {
-        EnterVehicleStandbyState();
       }
       else if (ButtonReleasedFlags != VEHICLE_SM_CTRL_ALL_BTN_RELEASED_FLAG)  /* hold until user release both buttons*/
       {
@@ -1871,7 +1870,7 @@ void Drive_VehicleStateMachine( void )
       { 
         if ((Btn_StateRead(BTN_IDX_BST_BTN)== BTN_STATE_HIGH) &&
             (Btn_StateRead(BTN_IDX_REV_BTN)== BTN_STATE_HIGH) &&
-            (Axis[0].ThrotMapping.PercentageTarget == 0 ) &&
+            (Axis[0].ThrotMapping.PercentageTarget < 0.01 ) &&
             (ABS(Axis[0].SpeedInfo.MotorMechSpeedRPM) < 100.0))
         {
           DualBtnTimeCnt ++;
@@ -1895,13 +1894,13 @@ void Drive_VehicleStateMachine( void )
       {
         EnterVehicleAlarmState();
       }
+	  else if( Btn_StateRead(BTN_IDX_KILL_SW) == BTN_STATE_HIGH)
+      {
+        EnterVehicleStandbyState();
+      }
       else if( ESCMainState == ESC_OP_NORMAL )
       {
         EnterVehicleDriveState();
-      }
-      else if( Btn_StateRead(BTN_IDX_KILL_SW) == BTN_STATE_HIGH)
-      {
-        EnterVehicleStandbyState();
       }
       else if (ButtonReleasedFlags != VEHICLE_SM_CTRL_ALL_BTN_RELEASED_FLAG)  /* hold until user release both buttons*/
       {
@@ -1925,7 +1924,7 @@ void Drive_VehicleStateMachine( void )
       { 
         if ((Btn_StateRead(BTN_IDX_BST_BTN)== BTN_STATE_HIGH) &&
             (Btn_StateRead(BTN_IDX_REV_BTN)== BTN_STATE_HIGH) &&
-            (Axis[0].ThrotMapping.PercentageTarget == 0 ) &&
+            (Axis[0].ThrotMapping.PercentageTarget < 0.01 ) &&
             (ABS(Axis[0].SpeedInfo.MotorMechSpeedRPM) < 100.0))
         {
           DualBtnTimeCnt ++;
