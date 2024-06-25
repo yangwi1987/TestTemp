6ecea1f1c4544cf1e7fdb38c59a70c4ad0ee644b
JeffChiaNingChang <chia-ning.chang@brp.com>
2024-06-25 20:28:01 +0800

    add FG square mode

--- r04010101_ESCAPP_20231004/MFModule/Src/FunctionGen.c
+++ r04010101_ESCAPP_20231004/MFModule/Src/FunctionGen.c
@@ -114,7 +114,66 @@ float FunctionGenerator_Sin_Calc( FG_t* v )
 
 float FunctionGenerator_Square_Calc( FG_t* v )
 {
-return 0;
+	float result = 0.0f;
+	if ( v->Start == 1 )
+	{
+		float temp_pos = 0.0f;
+		float t = 0.0f;
+		uint16_t quotient = 0;
+		t = (float)v->Step_Now * v->ExePeriod;
+		quotient = (uint16_t)(v->Freq_Now * t);//2PI * INV_2PI
+		temp_pos = ( _2PI * v->Freq_Now * t ) - (float)( quotient * _2PI );
+	    v->Signal_Now = (sin_LT[(uint16_t)(temp_pos * 4096.0f * INV_2PI + 0.5f)]) > 0.0f ? v->Amplitude : 0.0f;
+		v->Step_Now++;
+		if ( v->Direction == 1 )
+		{
+		    v->Freq_Now = v->Freq_Now + v->Freq_Step;
+
+		}
+		else
+		{
+			v->Freq_Now = v->Freq_Now - v->Freq_Step;
+		}
+
+		if ( v->Step_Now >= v->Total_Step )
+		{
+			switch ( v->Pattern )
+			{
+			    case FG_SIGNAL_PATTERN_ONCE:
+			    {
+			    	v->Start = 0;
+			    	v->Freq_Now = v->StartFreq;
+			    	break;
+			    }
+			    case FG_SIGNAL_PATTERN_REPEAT:
+			    {
+			    	v->Step_Now = 0;
+			    	v->Freq_Now = v->StartFreq;
+			    	break;
+			    }
+			    case FG_SIGNAL_PATTERN_UPDOWN:
+			    {
+			    	v->Step_Now = 0;
+			    	v->Freq_Now = v->EndFreq;
+			    	v->Direction = !v->Direction;
+			    	break;
+			    }
+			    default:
+			    {
+			    	break;
+			    }
+			}
+
+		}
+
+	}
+	else
+	{
+		v->Freq_Now = v->StartFreq;
+		v->Step_Now = 0;
+	}
+	result = v->Offset + v->Signal_Now;
+    return result;
 }
 float FunctionGenerator_Triangle_Calc( FG_t* v )
 {
