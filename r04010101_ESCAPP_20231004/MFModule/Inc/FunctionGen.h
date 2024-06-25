f496fe9827b937c5233e6c06b774c5b80ac1dced
JeffChiaNingChang <chia-ning.chang@brp.com>
2024-06-25 15:24:39 +0800

    implement function gen (1)

---
+++ r04010101_ESCAPP_20231004/MFModule/Inc/FunctionGen.h
@@ -0,0 +1,76 @@
+/*
+ * FunctionGen.h
+ *
+ *  Created on: 2024年6月25日
+ *      Author: Jeff C
+ */
+
+#ifndef INC_FUNCTIONGEN_H_
+#define INC_FUNCTIONGEN_H_
+
+#include "MathFunctionInclude.h"
+#include "Constant.h"
+
+typedef void (*functypeFunctionGenerator_Init)(void*,float,float,float,float,float,float);
+typedef float (*functypeFunctionGenerator_Calc)(void*);
+
+typedef enum
+{
+	FG_SIGNAL_SELECT_NONE = 0,
+	FG_SIGNAL_SELECT_SINE,
+	FG_SIGNAL_SELECT_SQUARE,
+	FG_SIGNAL_SELECT_TRIANGLE,
+	FG_SIGNAL_SELECT_ALL
+}FG_SIGNAL_SELECT_e;
+
+typedef enum
+{
+	FG_SIGNAL_PATTERN_ONCE = 0,
+	FG_SIGNAL_PATTERN_REPEAT,
+	FG_SIGNAL_PATTERN_UPDOWN,
+}FG_PATTERN_SELECT_e;
+
+typedef struct
+{
+	FG_SIGNAL_SELECT_e Signal_Select;
+	uint8_t Start;
+	FG_PATTERN_SELECT_e Pattern;
+	uint8_t Direction;
+	uint32_t Total_Step;
+	uint32_t Step_Now;
+    float Signal_Now;
+    float Freq_Now;
+    float Freq_Step;
+    float Offset;
+    float Amplitude;
+    float ExePeriod;
+	functypeFunctionGenerator_Init Init;
+	functypeFunctionGenerator_Calc Calc;
+}FG_t;
+
+#define FG_DEFAULT {\
+	FG_SIGNAL_SELECT_NONE,/*Signal_Select*/\
+	0,/*Start*/\
+	FG_SIGNAL_PATTERN_ONCE,/*Pattern*/\
+    0,/*Direction*/\
+	0,/*Total_Step*/\
+	0,/*Step_Now*/\
+	0.0f,/*Signal_Now*/\
+    0.0f,/*Freq_Now; */\
+    0.0f,/*Freq_Step;*/\
+    0.0f,/*Offset;   */\
+    0.0f,/*Amplitude;*/\
+    0.0f,/*ExePeriod;*/\
+	(functypeFunctionGenerator_Init)FunctionGenerator_Init,\
+	(functypeFunctionGenerator_Calc)FunctionGenerator_None_Calc,\
+}
+
+void FunctionGenerator_Init(FG_t* v, float Start_Freq, float End_Freq, float duration, float ExePeriod, float Amplitude, float offset);
+float FunctionGenerator_Sin_Calc( FG_t* v );
+float FunctionGenerator_Square_Calc( FG_t* v );
+float FunctionGenerator_Triangle_Calc( FG_t* v );
+float FunctionGenerator_None_Calc( FG_t* v );
+
+
+
+#endif /* INC_FUNCTIONGEN_H_ */
---
+++ r04010101_ESCAPP_20231004/MFModule/Src/FunctionGen.c
@@ -0,0 +1,120 @@
+/*
+ * FunctionGen.c
+ *
+ *  Created on: 2024年6月25日
+ *      Author: Jeff C
+ */
+
+
+#include "FunctionGen.h"
+
+
+void FunctionGenerator_Init(FG_t* v, float Start_Freq, float End_Freq, float duration, float ExePeriod, float Amplitude, float offset)
+{
+
+	v->Freq_Step = (( End_Freq - Start_Freq ) / duration ) * ExePeriod;
+	v->Total_Step = (uint32_t)( duration / ExePeriod );
+	v->Offset = offset;
+	v->Amplitude = Amplitude;
+	v->ExePeriod = ExePeriod;
+	v->Direction = End_Freq >= Start_Freq ? 1 : 0;
+	switch ( v->Signal_Select )
+	{
+	    case FG_SIGNAL_SELECT_SINE:
+	    {
+	    	v->Calc = (functypeFunctionGenerator_Calc)&FunctionGenerator_Sin_Calc;
+	    	break;
+	    }
+	    case FG_SIGNAL_SELECT_SQUARE:
+	    {
+	    	v->Calc = (functypeFunctionGenerator_Calc)&FunctionGenerator_Square_Calc;
+	    	break;
+	    }
+	    case FG_SIGNAL_SELECT_TRIANGLE:
+	    {
+	    	v->Calc = (functypeFunctionGenerator_Calc)&FunctionGenerator_Triangle_Calc;
+	    	break;
+	    }
+	    default:
+	    {
+	    	v->Calc = (functypeFunctionGenerator_Calc)&FunctionGenerator_None_Calc;
+	    	break;
+	    }
+	}
+
+}
+
+float FunctionGenerator_Sin_Calc( FG_t* v )
+{
+	float result = 0.0f;
+	if ( v->Start == 1 )
+	{
+		float temp_pos = 0.0f;
+		float t = 0.0f;
+		uint16_t quotient = 0;
+		t = (float)v->Step_Now * v->ExePeriod;
+		quotient = (uint16_t)(v->Freq_Now * t);//2PI * INV_2PI
+		temp_pos = ( _2PI * v->Freq_Now * t ) - (float)( quotient * _2PI );
+	    v->Signal_Now = sin_LT[(uint16_t)(temp_pos * 4096.0f * INV_2PI + 0.5f)];
+		v->Step_Now++;
+		if ( v->Direction == 1 )
+		{
+		    v->Freq_Now = v->Freq_Now + v->Freq_Step;
+
+		}
+		else
+		{
+			v->Freq_Now = v->Freq_Now - v->Freq_Step;
+		}
+
+		if ( v->Step_Now >= v->Total_Step )
+		{
+			switch ( v->Pattern )
+			{
+			    case FG_SIGNAL_PATTERN_ONCE:
+			    {
+			    	v->Start = 0;
+			    	break;
+			    }
+			    case FG_SIGNAL_PATTERN_REPEAT:
+			    {
+			    	v->Step_Now = 0;
+			    	break;
+			    }
+			    case FG_SIGNAL_PATTERN_UPDOWN:
+			    {
+			    	v->Step_Now = 0;
+			    	v->Direction = !v->Direction;
+			    	break;
+			    }
+			    default:
+			    {
+			    	break;
+			    }
+			}
+
+		}
+
+	}
+	else
+	{
+		v->Freq_Now = 0.0f;
+		v->Step_Now = 0;
+	}
+	result = v->Offset + v->Signal_Now;
+    return result;
+}
+
+float FunctionGenerator_Square_Calc( FG_t* v )
+{
+return 0;
+}
+float FunctionGenerator_Triangle_Calc( FG_t* v )
+{
+	return 0;
+}
+float FunctionGenerator_None_Calc( FG_t* v )
+{
+//nothing happen
+	return 0;
+}
